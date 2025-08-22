301ef2db1928c5e06d277cd234ce1c37f15a062c
IDDI5996 <iddibakari171@gmail.com>
2025-08-22 16:30:42 +0300

    Updated contactus

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -46,10 +46,7 @@
 
             <!-- Google Map -->
             <div>
-                <iframe
-                    class="w-full h-64 rounded shadow"
-                    src="https://www.google.com/maps/embed?pb=YOUR_GOOGLE_MAP_EMBED_CODE_HERE"
-                    allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                <iframe class="w-full h-64 rounded shadow" src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d4700.815495000526!2d39.220511566795125!3d-6.181283810025378!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNsKwMTAnNTIuOSJTIDM5wrAxMycxNC4xIkU!5e0!3m2!1sen!2stz!4v1755868810847!5m2!1sen!2stz" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
         </div>
 
